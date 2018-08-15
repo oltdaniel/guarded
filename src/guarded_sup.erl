9998654acee0fe06b6bb1b6216d18da3b7fbb056
Daniel Oltmanns <oltmannsdaniel@outlook.com>
2018-08-15 21:08:14 +0200

    server: More reliable handling
    If one connection fails, any other connection will be kept open.

--- src/guarded_sup.erl
+++ src/guarded_sup.erl
@@ -10,17 +10,18 @@
 -export([start_link/0]).
 -export([init/1]).
 
--define(SERVER, ?MODULE).
-
 %
 %% Start the supervisor for the application
 %
 start_link() ->
-  guarded_messenger:start_link(),
-  supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 %
 %% Initialize placeholder
 %
 init([]) ->
-  {ok, { {one_for_all, 0, 1}, []} }.
+  {ok, { {one_for_one, 5, 10}, [
+    {guarded_messenger, {
+      guarded_messenger, start_link, []
+    }, permanent, 5000, worker, []}
+  ]} }.
