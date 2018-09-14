2d78e38b525ea8a0bf6911d2945a35ee41584986
Daniel Oltmanns <oltmannsdaniel@outlook.com>
2018-09-14 20:50:31 +0200

    server: Cleaned up code and commented
    The server code has been cleaned up and commented to be understood easier.

--- src/guarded_ws_handler.erl
+++ src/guarded_ws_handler.erl
@@ -2,9 +2,24 @@
 %% Define module properties
 %
 -module(guarded_ws_handler).
-% -behaviour(cowboy_http_websocket_handler).
 
--record(state, {uid, receiver_uid, public_key, shared_key, secret, prime, generator}).
+%
+%% Additional user socket information
+%
+-record(state, {
+  uid,          % Store the socket identified
+  receiver_uid, % Current session partner
+  public_key,   % Public key of the socket
+  shared_key,   % The shared key of the key exchange
+  secret,       % Server secret selected for the key exchange
+  generator     % Generator number selected for the key exchange
+}).
+
+%
+%% Some crypto settings
+%
+-define(SHARED_PRIME, 50053).    % The prime selected for the key exchange
+-define(STREAM_CIPHER, aes_ctr). % The cipher used for the stream encryption
 
 %
 %% Make functions public
@@ -18,26 +33,24 @@
 %% Entry function for a new socket
 %
 init(Req, _Opts) ->
-  Prime = 50053,
   Generator = crypto:bytes_to_integer(crypto:strong_rand_bytes(1)),
   Secret = crypto:bytes_to_integer(crypto:strong_rand_bytes(2)),
-  SharedKey = crypto:bytes_to_integer(crypto:mod_pow(Generator, Secret, Prime)),
-  State = #state{shared_key=SharedKey, secret=Secret, prime=Prime, generator=Generator},
+  SharedKey = crypto:bytes_to_integer(crypto:mod_pow(Generator, Secret, ?SHARED_PRIME)),
+  State = #state{shared_key=SharedKey, secret=Secret, generator=Generator},
   crypto:start(),
   {cowboy_websocket, Req, State}.
 
 %
-%% Mix up socket on connection
+%% Initialize the socket
 %
 websocket_init(State) ->
   SharedKey = State#state.shared_key,
-  Prime = State#state.prime,
   Generator = State#state.generator,
-  Msg = lists:concat(["shared:", SharedKey, " ", Prime, " ", Generator]),
+  Msg = lists:concat(["shared:", SharedKey, " ", ?SHARED_PRIME, " ", Generator]),
   {reply, {text, Msg}, State}.
 
 %
-%% Handle text messages
+%% Handle new user id request
 %
 websocket_handle({parsed, <<"/s", _Msg/binary>>}, State) ->
   guarded_messenger:disconnect(State#state.uid),
@@ -46,34 +59,57 @@ websocket_handle({parsed, <<"/s", _Msg/binary>>}, State) ->
   guarded_messenger:connect(Uid, self()),
   websocket_info({message, <<"uid">>, Uid}, State2);
 
+%
+%% Handle send message
+%
 websocket_handle({parsed, <<"/p ", Msg/binary>>}, State) ->
   Uid = State#state.uid,
   ReceiverUid = State#state.receiver_uid,
   guarded_messenger:message(Uid, ReceiverUid, Msg),
   {ok, State};
 
+%
+%% Handle new partner selection
+%
 websocket_handle({parsed, <<"/o ", Msg/binary>>}, State) ->
   State2 = State#state{receiver_uid = Msg},
   websocket_info({message, <<"server">>, <<"speaking to ", Msg/binary>>}, State2);
 
+%
+%% Handle key publish
+%
 websocket_handle({parsed, <<"/k ", Msg/binary>>}, State) ->
   State2 = State#state{public_key = Msg},
   {ok, State2};
 
+%
+%% Handle key request message
+%
 websocket_handle({parsed, <<"/kr ", Msg/binary>>}, State) ->
   Uid = State#state.uid,
   guarded_messenger:key_request(Uid, Msg),
   {ok, State};
 
+%
+%% handle ping message
+%
 websocket_handle({text, <<"ping", _Msg/binary>>}, State) ->
   {ok, State};
 
+%
+%% Handle Key Exchange message
+%
 websocket_handle({text, <<"/d ", Msg/binary>>}, State) ->
-  SharedKey = crypto:bytes_to_integer(crypto:mod_pow(binary_to_integer(Msg), State#state.secret, State#state.prime)),
-  SharedKey2 = crypto:hash(sha256, integer_to_list(SharedKey, 16)),
-  State2 = State#state{shared_key=SharedKey2},
+  ClientPart = binary_to_integer(Msg),
+  SharedKey = crypto:bytes_to_integer(crypto:mod_pow(ClientPart, State#state.secret, ?SHARED_PRIME)),
+  SharedKey2 = integer_to_list(SharedKey, 16),
+  SharedKey3 = crypto:hash(sha256, SharedKey2),
+  State2 = State#state{shared_key=SharedKey3},
   {ok, State2};
 
+%
+%% Handle general text messages
+%
 websocket_handle({text, Msg}, State) ->
   Decoded = base64:decode(<<Msg/binary>>),
   Msg2 = binary_to_list(Decoded),
@@ -83,6 +119,9 @@ websocket_handle({text, Msg}, State) ->
   {_, Msg4} = crypto:stream_decrypt(CryptoStream, Msg3),
   websocket_handle({parsed, Msg4}, State);
 
+%
+%% Handle undefined messages
+%
 websocket_handle(_Msg, State) ->
   websocket_info({message, <<"server">>, <<"undefined command">>}, State),
   {ok, State};
@@ -110,7 +149,8 @@ websocket_info({message, Uid, Msg}, State) ->
 websocket_info({key_request, ReceiverUid}, State) ->
   Uid = State#state.uid,
   PublicKey = State#state.public_key,
-  guarded_messenger:message(<<"key">>, ReceiverUid, << Uid/binary, " ", PublicKey/binary >>),
+  Msg = << Uid/binary, " ", PublicKey/binary >>,
+  guarded_messenger:message(<<"key">>, ReceiverUid, Msg),
   {ok, State};
 
 %
